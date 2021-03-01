b1455704111e9f22bab8770bde6060818923bce4
Artemov Anton <kuith@yandex-team.ru>
2021-03-01 04:20:32 +0300

    dynamic parser

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import { promises } from 'fs';
+import chalk from 'chalk';
 
 import { startBrowser, openPage } from './browser.js';
-import { URL, parser } from './parsers/noita_perks.js';
 
 const { writeFile } = promises;
+const log = console.log;
 
 const main = async () => {
+    let file = process.argv[2];
+    if (!file) {
+        log(`
+${chalk.red('specify filename from ./parsers dir')}
+${chalk.red('for example')} ${chalk.black.bgWhite('yarn start -- noita_perks')}
+`);
+        process.exit();
+    }
+    if (!/.+\.js/.test(file)) {
+        file += '.js';
+    }
+    const { URL, parser } = await import(`./parsers/${file}`);
+
     const browser = await startBrowser();
 
     const page = await openPage(browser, URL);
-    await page.screenshot({ path: 'test.png' });
+    // await page.screenshot({ path: 'test.png' });
 
     let data = await page.evaluate(parser);
 
